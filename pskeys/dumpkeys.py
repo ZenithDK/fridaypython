cbf9c21cc27fa4e8b535a04cc8303faed47dbe9b
Johann Fridriksson <johann.fridriksson@gmail.com>
2016-04-01 17:11:22 +0200

    Bug in dumpkeys.py, dumped all entries multiple times

--- pskeys/dumpkeys.py
+++ pskeys/dumpkeys.py
@@ -8,13 +8,12 @@ all_entries = pickle.load(open("pskeys.p", "rb"))
 print_order = ["NAME", "SUMMARY", "TYPE", "DEFAULT VALUE", "DESCRIPTION", "SECOM_KEY", "SECOM_DESCRIPTION"]
 
 for entry in all_entries:
-    for key in entry.keys():
-        print ""
-        for print_key in print_order:
-            try:
-                section_text = entry[print_key.lower()]
-                print print_key.replace("_", " ")
-                print "\t%s"%section_text.strip()
-            except KeyError:
-                pass
-        print "="*78
+    print ""
+    for print_key in print_order:
+        try:
+            section_text = entry[print_key.lower()]
+            print print_key.replace("_", " ")
+            print "\t%s"%section_text.strip()
+        except KeyError:
+            pass
+    print "="*78
